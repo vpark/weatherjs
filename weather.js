ef2db3942edfd6497b29c4d75ad05e35cdc0cc2f
Vincent Park <vincentpark@gmail.com>
2014-02-17 17:55:51 -0800

    working on css

--- weather.js
+++ weather.js
@@ -1,34 +1,42 @@
-
 function mainWeather() {
-  navigator.geolocation.getCurrentPosition(locationTrue, locationFalse);
+  navigator.geolocation.getCurrentPosition(locationSucceed, locationFail);
+  var jsonURL;
+  var jsonData;
   
-  function locationTrue(position) {
+  function locationSucceed(position) {
     var lon = position.coords.longitude;
     var lat = position.coords.latitude;
-    
-    var api = "http://openweathermap.org/data/2.5/weather?/&cnt=7&lon=" + lon + "&lat=" + lat + "&APPID=01df5cdb26af96d7d47a0a6c01dd7fa3"
+    jsonURL = "http://api.openweathermap.org/data/2.5/forecast/daily?&cnt=7&mode=json&lon=" + lon + "&lat=" + lat + "&APPID=01df5cdb26af96d7d47a0a6c01dd7fa3"
     
     $('#latitude').html(lat);
     $('#longitude').html(lon);
-    
-    
-    $.ajax({
-      url: api,
-      data: { get_param: 'value' }, 
-      dataType: 'json',
-      success: function(data){
-        var main = data.list[0].weather[0].main;
-        var temp = data.main.temp;
-
-        $('#main').html(main);
-        $('#temp').html(temp);
-      }
-    });
+         
+    jsonRequest();
   }
   
-  function locationFalse(){
+  function jsonRequest() {
+    $.getJSON(
+      jsonURL, function(json){
+        jsonData = json;
+        weatherPopulate();
+      }
+    );
+  }
+  
+  function locationFail(){
     alert("what is this i dont even");
   }
+  
+  function weatherPopulate(){
+    console.log(jsonURL);
+    console.log(jsonData);
+    var weatherDesc = jsonData.list[0].weather[0].main;
+    var temp = jsonData.list[0].temp.day;
+
+    $('#weatherDesc').html(weatherDesc);
+    $('#temp').html(temp);
+  }
+        
 }
 
 $(document).ready(mainWeather());
